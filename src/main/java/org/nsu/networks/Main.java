c35cfd816259e746772960760933a6fb0d7c9fb2
i.peretyatko <i.peretyatko@g.nsu.ru>
2024-11-03 20:12:30 +0700

    created repository

---
+++ src/main/java/org/nsu/networks/Main.java
@@ -0,0 +1,16 @@
+package org.nsu.networks;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+}
