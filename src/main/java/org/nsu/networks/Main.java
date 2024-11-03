f1d1319b8d6b427c4e44ac537dc08e004edfa8a0
i.peretyatko <i.peretyatko@g.nsu.ru>
2024-11-04 02:26:51 +0700

    add protobuf

--- src/main/java/org/nsu/networks/Main.java
+++ src/main/java/org/nsu/networks/Main.java
@@ -1,16 +1,26 @@
 package org.nsu.networks;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println(11);
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/root.fxml")));
+        Scene scene = new Scene(root, 900, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
 }
