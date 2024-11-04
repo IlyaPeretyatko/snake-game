4b49417e766803dddb235ca1e2e785aff5da8b1b
i.peretyatko <i.peretyatko@g.nsu.ru>
2024-11-04 19:50:38 +0700

    added menuController and set trigger on clicking exit

---
+++ src/main/java/org/nsu/networks/controllers/MenuController.java
@@ -0,0 +1,30 @@
+package org.nsu.networks.controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button connectButton;
+
+    @FXML
+    private Button optionsButton;
+
+    @FXML
+    private Button exitButton;
+
+    public void exitButtonClicked(MouseEvent mouseEvent) {
+        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        window.close();
+    }
+
+
+}
