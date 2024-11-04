ddd4188a31dedd05abef6a37872734aaaedf4d39
i.peretyatko <i.peretyatko@g.nsu.ru>
2024-11-04 23:04:20 +0700

    add dialog box error

--- src/main/java/org/nsu/networks/controllers/MenuController.java
+++ src/main/java/org/nsu/networks/controllers/MenuController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class MenuController {
 
@@ -28,7 +30,16 @@ public class MenuController {
     }
 
     public void newGameButtonClicked(MouseEvent mouseEvent) {
+        String name = nameTextField.getText();
+        if (name.isEmpty()) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Nickname cannot be empty");
+            alert.setContentText("Write your nickname");
+            alert.showAndWait();
+        } else {
 
+        }
     }
 
     public void connectButtonClicked(MouseEvent mouseEvent) {
